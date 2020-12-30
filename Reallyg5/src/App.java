3f19c064f50efce47332fa0e595657a2e3c56b00
AnthonyRicse98 <73305764+AnthonyRicse98@users.noreply.github.com>
2020-12-30 01:15:15 -0500

    yara

--- Reallyg5/src/App.java
+++ Reallyg5/src/App.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 import controlador.ControladorPantalla;
 import modelo.Usuario;
 import vista.frmCel;
@@ -16,21 +16,4 @@ public class app {
       
     }
 }
-=======
 
-import controlador.ControladorPantalla;
-import vista.frmCel;
-import modelo.UsuarioArreglo;
-import modelo.Usuario;
-import sistema.Datos;
-
-public class App {
-    public static void main(String[] args) {
-      Datos.usuarios.add(new Usuario("dokouser98@gmail.com", "4932665"));
-      frmCel fcel = new frmCel();
-      
-      ControladorPantalla controlaPant = new ControladorPantalla(Datos.usuarios,fcel);
-      controlaPant.iniciar();
-    }
-}
->>>>>>> Stashed changes
